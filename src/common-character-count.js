8d1f945ab60c9c7ceec403366b8debc26ae16861
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-02 14:03:16 +0400

    do count of letters task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let map1 = createMap(s1);
+  let map2 = createMap(s2);
+
+  function createMap (word) {
+    let arr = word.split('');
+    let map = new Map();
+      for(let element of arr) {
+        let count = 0;
+        for(let otherElement of arr) {
+          if (element === otherElement) {
+            count++;
+          }
+        }
+        map.set(element, count)
+      }
+    return map;
+  }
+
+  let num = 0;
+  for (let letter of map1.keys()){
+    for (let secondLetter of map2.keys()) {
+      if (letter === secondLetter) {
+        if (map1.get(letter) < map2.get(secondLetter)){
+          num += map1.get(letter);
+        } else {
+          num += map2.get(secondLetter);
+        }
+      }
+    }
+  }
+  console.log(num)
+  return num;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let str = n.toString(10);
+  if(str.length <= 1) {
+    console.log(n); // показывает 2
+    return n;
+  }
+
+  let arr = str.split('');
+  let arrNew = [];
+  for(let element of arr) {
+    element = parseInt(element, 10);
+    arrNew.push(element);
+  }
+  let result = arrNew.reduce((sum, current) => sum + current, 0);
+
+  getSumOfDigits(result);
 }
 
+console.log(getSumOfDigits(245)); // показывает undefined
+
+/*
 module.exports = {
   getSumOfDigits
 };
+*/
