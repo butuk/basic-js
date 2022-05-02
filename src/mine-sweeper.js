7a4f6e2dcbc5b4322b94ccfde5def73d33ca7266
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-02 15:13:11 +0400

    do mine sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  let resultMatrix = [];
+  for (let i = 0, rows = matrix.length; i < rows; i++) {
+    resultMatrix.push([]);
+  }
+
+  for (let i = 0, rows = matrix.length; i < rows; i++) {
+    for(let j = 0, columns = matrix[i].length; j < columns; j++) {
+      let minesNum = 0;
+      if(i > 0 && j > 0 && matrix[i-1][j-1]){
+        minesNum++;
+      }
+      if(i > 0 && matrix[i-1][j]){
+        minesNum++;
+      }
+      if(i > 0 && j < (columns - 1) && matrix[i-1][j+1]){
+        minesNum++;
+      }
+      if(j > 0 && matrix[i][j-1]){
+        minesNum++;
+      }
+      if(j < (columns - 1) && matrix[i][j+1]){
+        minesNum++;
+      }
+      if(i < (rows - 1) && j > 0 && matrix[i+1][j-1]){
+        minesNum++;
+      }
+      if(i < (rows - 1) && matrix[i+1][j]){
+        minesNum++;
+      }
+      if (i < (rows - 1) && j < (columns - 1) && matrix[i+1][j+1]){
+        minesNum++;
+      }
+      resultMatrix[i][j] = minesNum;
+    }
+  }
+
+  return resultMatrix;
 }
+
+/*console.log(minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+]))*/
 
 module.exports = {
   minesweeper
