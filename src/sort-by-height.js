85615129d4e00fe3af960d41a1a109ec74e79618
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-02 16:50:08 +0400

    do sort by height task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,10 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr ) {
+  //throw new NotImplementedError('Not implemented');
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  };
+
+  let tempArr = arr.filter(item => item !== -1).sort(compareNumeric);
+  let resultArr = [];
+
+  for (let i = 0, length = arr.length; i < length; i++) {
+    if(arr[i] === -1) {
+      resultArr.push(-1);
+    } else  {
+      resultArr.push(tempArr[0]);
+      tempArr.shift();
+    }
+  }
+
+  return resultArr;
 }
+
+//console.log(sortByHeight([-1, 150, 160, 170, -1, -1, 180, 190]));
 
 module.exports = {
   sortByHeight
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
+
+  if(!(arr instanceof Array)) {
+    throw Error ("'arr' parameter must be an instance of the Array!");
+  }
+
+  let resultArr = [];
+
+  for (let i = 0, total = arr.length; i < total; i++) {
+    if (arr[i] === '--discard-prev') {
+      if(i > 0) {
+        resultArr.pop();
+      }
+    } else if (arr[i] === '--double-prev') {
+      if(i > 0) {
+        resultArr.push(arr[i-1]);
+      }
+    } else if (arr[i] === '--double-next') {
+      if(i < total-1) {
+        resultArr.push(arr[i+1]);
+      }
+    } else if (arr[i] === '--discard-next') {
+      if(i < total-1){
+        arr.splice(i, 1);
+      }
+    } else {
+      resultArr.push(arr[i]);
+    }
+  }
+
+  return resultArr;
 }
+
+//console.log(transform([1, 2, 3, '--discard-next', 4, 5]));
 
 module.exports = {
   transform
